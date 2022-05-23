226a68f7011cee9e20119c1f66d7191f3144971f
tetA <doublepotato-temporarygittempo@yahoo.co.jp>
2022-05-23 23:19:42 +0900

    課題に取り組む前

---
+++ Instagram/Const.swift
@@ -0,0 +1,17 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by 矢野俊作 on 2022/05/17.
+//
+
+import Foundation
+
+struct Const {
+    
+    //ImagePathは、Storage内の画像ファイルの保存場所を定義しています。
+    static let ImagePath = "images"
+    
+    //PostPathは、Firestore内の投稿データ（投稿者名、キャプション、投稿日時等）の保存場所を定義しています。
+    static let PostPath = "posts"
+}
